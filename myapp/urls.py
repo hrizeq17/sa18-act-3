654683f837edd63cb7cf6c1eea71e88b148e2682
hrizeq17 <hrizeq@memphis.edu>
2024-04-27 16:31:54 -0500

    fixing the url

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('products/', views.index, name='index'),
     path('products/<int:id>/', views.show, name='show'),
 ]
